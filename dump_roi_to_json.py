fb9449ad2a0ff6fef1eaf4c4748bd4b5ebe77846
Nishon Tandukar <nishon.tan@gmail.com>
2019-05-14 11:50:27 +0545

    [#1] bb exported to json file when s key is pressed" "

--- dump_roi_to_json.py
+++ dump_roi_to_json.py
@@ -4,6 +4,7 @@ import imutils
 import time
 import cv2
 import os
+import json
 from src.custom_args_parse import str2bool
 from src.bounding_box import BoundingBox
 
@@ -13,6 +14,7 @@ bounding_boxes = []
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", type=str, help="path to input video file")
+ap.add_argument("-e", "--export", type=str,default='./output.json', help="path to export")
 ap.add_argument("-p", "--paused", type=str2bool, nargs='?',
                 const=True, default="yes", help="Pauses on first frame")
 
@@ -42,15 +44,22 @@ def center_of_rect(x, y, w, h):
 
 
 def show_roi_selector(frame):
-    initBB = cv2.selectROI("Frame", frame, fromCenter=False,
-                           showCrosshair=True)
-    (startX, startY, w, h) = initBB
+    bb = cv2.selectROI("Frame", frame, fromCenter=False,
+                       showCrosshair=True)
+    (startX, startY, w, h) = bb
     endX = startX + w
     endY = startY + h
     centroids.append(center_of_rect(startX, startY, w, h))
-    bounding_boxes.append(initBB)
 
-    # Add trackers here
+    bounding_box_uid = len(bounding_boxes)
+    frame_number = 1  # todo: make this value dynamic
+    bounding_boxes.append(BoundingBox(bounding_box_uid, bb, frame_number))
+
+
+def save_drawn_bounding_boxes():
+    json_string = json.dumps([ob.__dict__ for ob in bounding_boxes])
+    with open(args['export'], "w") as text_file:
+        print(json_string, file=text_file)
 
 
 def pause_on_current_frame(frame):
@@ -67,17 +76,25 @@ def draw_circle(frame):
             frame, (val[0], val[1]), 4, (0, 255, 0), -1)
 
 
+def draw_text(frame,coords,text):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, str(text), coords, font,
+                0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+
 def clear_all_trackers():
     global centroids
     centroids = []
 
 
 def draw_bounding_boxes(frame):
-    for bb in bounding_boxes:
-        (startX, startY, w, h) = bb
+    for obj in bounding_boxes:
+        (startX, startY, w, h) = obj.bounding_box
         endX = startX + w
         endY = startY + h
-        cv2.rectangle(frame, (startX,startY),(endX,endY),(255,0,0), 2)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 0, 0), 2)
+        draw_text(frame,
+                  center_of_rect(startX, startY, w, h),obj.object_id)
 
 
 def main():
@@ -91,19 +108,17 @@ def main():
             frame = paused_frame
         else:
             frame = load_one_frame(video_stream)
-        
+
         # Exit if there not video
         if frame is None:
             print("video stream has ended")
             break
-        
+
         frame = imutils.resize(frame, width=1000)
 
         if args["paused"] and paused_frame is None:
             pause_on_current_frame(frame)
             continue
-
-
 
         draw_bounding_boxes(frame)
 
@@ -116,6 +131,8 @@ def main():
             show_roi_selector(frame)
         if key == ord('c'):
             clear_all_trackers()
+        if key == ord('s'):
+            save_drawn_bounding_boxes()
 
         if key == 27:
             break
--- src/bounding_box.py
+++ src/bounding_box.py
@@ -1,5 +1,13 @@
-class BoundingBox:
+from .serializer import Serializable
+import json
+
+
+class BoundingBox():
 	def __init__(self, object_id, bounding_box,frame_no):
 		self.object_id = object_id
 		self.bounding_box = bounding_box
-		self.frame_no = frame_no
+		self.frame_no = frame_no	
+
+
+	def __repr__(self):
+		return json.dumps(self.__dict__)
